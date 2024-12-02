bc49e7e2c2088ecf8e1a6fe8b18c78c42aa8b2d6
ChathuraRubasingha <79496529+ChathuraRubasingha@users.noreply.github.com>
2024-12-02 22:02:05 +0530

    notification design changed

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,28 +1,27 @@
-import React, { useEffect } from 'react';
-import '../styles/Notification.css';
-import correct from '../assets/Icons/g-correct.png'
+import React, { useEffect } from "react";
+import "../styles/Notification.css";
+import correct from "../assets/Icons/g-correct.png";
+import { TickCircle } from "iconsax-react";
 
 const Notification = ({ message, onClose }) => {
-    useEffect(() => {
-      const timer = setTimeout(() => {
-        onClose();
-      }, 3000);
-  
-      return () => clearTimeout(timer);
-    }, [onClose]);
-  
-    if (!message) return null;
-  
-    return (
-        <div className="notification-overlay">
-        <div className="notification-popup">
-        <div className='n-icon'>
-            <img src={correct} alt='correct-icon'/>
-        </div>
-          <p>{message}</p>
-        </div>
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onClose();
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [onClose]);
+
+  if (!message) return null;
+
+  return (
+    <div className="notification-overlay">
+      <div className="notification-popup">
+        <TickCircle size="32" color="green" variant="Bold" />
+        <p>{message}</p>
       </div>
-    );
-  };
-  
-  export default Notification;
+    </div>
+  );
+};
+
+export default Notification;
